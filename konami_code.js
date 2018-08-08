b5080aa59895410846922ab332f288433bf93ae4
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-08 18:21:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  function init() {
+  let index = 0
 
+  document.body.addEventListener('keydown', function(e) {
+    if (e.key === codes[index]) {
+      index++
+      console.log(e.key)
+      console.log(index)
+      if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
+}
 }
